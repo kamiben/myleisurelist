6bbc305b211e8c779d08d8c8699eccc8bbb06b69
Kamiben <eadel@arthion.fr>
2010-09-18 10:12:52 +0200

    Création de l'appli myleisurelist

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Myleisurelist::Application.config.secret_token = 'c470a13dc6fc1327e7e01fd94a31b467c89d21ee02a46f72861d6ae4214046cb27241ebceba14425d799ca100761692f70a0d2b458e970f4892d907d38ff3987'
