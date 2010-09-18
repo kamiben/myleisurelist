b65f83864f9bc6d89256c98752691d3eac9460fe
Kamiben <eadel@arthion.fr>
2010-09-18 15:49:52 +0200

    Lien entre media et consulte

--- app/models/consulte.rb
+++ app/models/consulte.rb
@@ -1,2 +1,4 @@
 class Consulte < ActiveRecord::Base
+  belongs_to :media
+  #belongs_to :user #pas encore implémenté
 end
--- app/models/media.rb
+++ app/models/media.rb
@@ -1,3 +1,4 @@
 class Media < ActiveRecord::Base
   validates_presence_of :nom, :categorie
+  has_many :consultes
 end
