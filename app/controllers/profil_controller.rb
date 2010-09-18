cd736333800c4fbca0457058121902cfeac45810
Kamiben <eadel@arthion.fr>
2010-09-18 23:04:33 +0200

    création page profil d'un utilisateur

--- app/controllers/profil_controller.rb
+++ app/controllers/profil_controller.rb
@@ -1,5 +1,9 @@
 class ProfilController < ApplicationController
+  before_filter :authenticate_user!
+ 
   def show
+    @user = current_user
+    @media_list = current_user.medias_consultes
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,8 @@ class User < ActiveRecord::Base
   attr_accessible :email, :password, :password_confirmation, :remember_me
   
   has_many :consulte
+  has_many :medias_consultes,
+    :through => :consulte, 
+    :source => :media 
+
 end
