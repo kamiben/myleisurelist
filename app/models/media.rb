fe1abaa2817294a6fa7ea49c708692a7c5cf9252
Kamiben <eadel@arthion.fr>
2010-09-18 15:55:50 +0200

    url clean pour un media

--- app/models/media.rb
+++ app/models/media.rb
@@ -1,4 +1,9 @@
 class Media < ActiveRecord::Base
   validates_presence_of :nom, :categorie
   has_many :consultes
+  
+    def to_param
+      "#{id}-#{nom.gsub(/\W/, '-').downcase}"
+    end
+
 end
